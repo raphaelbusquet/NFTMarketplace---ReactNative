1c49fafcd46fa34efa49bfb66c657cad3c335251
Raphael Busquet <raphaelbusquet@outlook.com>
2023-06-06 20:33:18 +0100

    done NFTCard

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,38 @@
-import { Text } from "react-native"
+import { useState } from "react"
+import { Text, View, SafeAreaView, FlatList } from "react-native"
+
+import { COLORS, NFTData } from '../constants'
+import { NFTCard, HomeHeader, FocusedStatusBar } from '../components'
 
 const Home = () => {
   return (
-    <Text>Home</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
+
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 1 }}>
+          <FlatList 
+            data={NFTData}
+            renderItem={({ item }) => <NFTCard data={item}/>}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+          />
+        </View>
+
+        <View style={{
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          zIndex: -1,
+        }}>
+          <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
